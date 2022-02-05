43980cbfcd5095a3982a9557192528425277f88c
wdoumerg <89606153+wdoumerg@users.noreply.github.com>
2022-02-05 15:39:49 +0100

    Update Helicities_ZZ.cpp

--- Fpmc/External/excl_aaaa/commons/Helicities_ZZ.cpp
+++ Fpmc/External/excl_aaaa/commons/Helicities_ZZ.cpp
@@ -2980,7 +2980,7 @@ double dsigma_f(double s, double theta)
 
 double dsigma_ZZ(double s, double t,int exclude_loop)
 {
-     if (s/(4*mZ2)<10)
+     if (s/(4*mZ2)<1)
      {
           return 0;
      }
@@ -3010,7 +3010,7 @@ double dsigma_ZZ(double s, double t,int exclude_loop)
      {
           return 0;
      }
-     double coeff_W=0;
+     double coeff_W=1;
      double coeff_f=1;
      double coeff_exotic_f=0;
      double coeff_Higgs=1;
